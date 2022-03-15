0d2dd55291bef2d4d334f1816265da653eea8d28
Gaspar Rebord <gasparrebord31@gmail.com>
2022-03-15 13:45:05 -0300

    Problemas con las promise

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import ItemCounter from './ItemCounter';
 
-const Item = () => {
+const Item = ({ key, name, type, description, atk, def, level, race, attribute, archetype, image }) => {
     return (
         <div className='item'>
-            <img className='itemImg' src="https://http2.mlstatic.com/D_NQ_NP_829321-MLA46348271392_062021-O.webp" alt="" />
+            <img className='itemImg' src={image} alt={key} />
             <div className='itemDescription'>
-                <h5 className='itemTitle'>Smart Tv Noblex Dk50x6500 Led 4k 50''</h5>
-                <p className='itemPrice'>$ 67300</p>
+                <h5 className='itemTitle'>{name}</h5>
+                <div className='itemStats'>
+                    <p>[{race}  -  {type}]</p>
+                    <p>ATK/{atk}  -  DEF/{def}</p>
+                </div>
+
+                <p className='itemDesc'>{description}</p>
                 <ItemCounter />
             </div>
         </div>
--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Item from './Item';
 
-const ItemList = () => {
+const ItemList = ({ cardList }) => {
     return (
-        <div className='ItemList'>
-            <Item />
+        <div className='itemList'>
+            {cardList.map((cards) =>
+                <Item key={cards.id} name={cards.name} type={cards.type} description={cards.desc} atk={cards.atk} def={cards.def} level={cards.level} race={cards.race} attribute={cards.attribute} archetype={cards.archetype} image={cards.image_url}
+                />)}
         </div>
     )
 };
